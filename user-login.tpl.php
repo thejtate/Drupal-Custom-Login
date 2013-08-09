55008df64a341bfeb7a96b283f9d8a5f62275506
Josh Tate <thejtate@gmail.com>
2013-08-09 17:02:13 -0500

    Added default user-login build

---
+++ user-login.tpl.php
@@ -0,0 +1,7 @@
+<?php 
+  print drupal_render($form['name']);
+  print drupal_render($form['pass']);
+  print drupal_render($form['form_build_id']);
+  print drupal_render($form['form_id']);
+  print drupal_render($form['actions']);
+?>
