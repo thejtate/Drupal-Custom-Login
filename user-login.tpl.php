88d8c5d8a37067509400e1fe4f8e3b85d1f0718b
Josh Tate <jtate@funneldesigngroup.com>
2013-09-25 09:59:48 -0500

    Merge dev to master

--- user-login.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  print drupal_render($form['name']);
-  print drupal_render($form['pass']);
-  print drupal_render($form['form_build_id']);
-  print drupal_render($form['form_id']);
-  print drupal_render($form['actions']);
-?>
