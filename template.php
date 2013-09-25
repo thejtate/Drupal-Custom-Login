4f2027344cd550c7bdba63970d382347c1e38971
Josh Tate <jtate@funneldesigngroup.com>
2013-09-25 09:57:28 -0500

    Updated hook

--- template.php
+++ template.php
@@ -1,7 +1,6 @@
 <?php
-/*
- *  Remove labels and add HTML5 placeholder attribute to login form
- */
+
+/*Remove labels and add HTML5 placeholder attribute to login form*/
 function THEME_form_alter(&$form, &$form_state, $form_id) {
   if ( TRUE === in_array( $form_id, array( 'user_login', 'user_login_block') ) )
     $form['name']['#attributes']['placeholder'] = t( 'Username' );
@@ -10,11 +9,7 @@ function THEME_form_alter(&$form, &$form_state, $form_id) {
     $form['pass']['#title_display'] = "invisible";
 }
 
-
-
-/*
- *  Remove login form descriptions
- */
+/*Remove login form descriptions */
 function THEME_form_user_login_alter(&$form, &$form_state) {
     $form['name']['#description'] = t('');
     $form['pass']['#description'] = t('');
