89e5f17427eaf776bed370e256bd7080d6870106
Josh Tate <jtate@funneldesigngroup.com>
2013-09-25 09:47:41 -0500

    Updated template

---
+++ page--user-login.tpl.php
@@ -0,0 +1,4 @@
+<?php print $messages; ?>
+
+<?php print drupal_render(drupal_get_form('user_login')); ?>
+<a href="user/password">Request new password"</a>
--- template.php
+++ template.php
@@ -1,15 +1,22 @@
-<?
-function yourthemename_theme() {
-  $items = array();
-  // create custom user-login.tpl.php
-  $items['user_login'] = array(
-  'render element' => 'form',
-  'path' => drupal_get_path('theme', 'yourthemename') . '/templates',
-  'template' => 'user-login',
-  'preprocess functions' => array(
-  'yourthemename_preprocess_user_login'
-  ),
- );
-return $items;
+<?php
+/*
+ *  Remove labels and add HTML5 placeholder attribute to login form
+ */
+function THEME_form_alter(&$form, &$form_state, $form_id) {
+  if ( TRUE === in_array( $form_id, array( 'user_login', 'user_login_block') ) )
+    $form['name']['#attributes']['placeholder'] = t( 'Username' );
+    $form['pass']['#attributes']['placeholder'] = t( 'Password' );
+    $form['name']['#title_display'] = "invisible";
+    $form['pass']['#title_display'] = "invisible";
+}
+
+
+
+/*
+ *  Remove login form descriptions
+ */
+function THEME_form_user_login_alter(&$form, &$form_state) {
+    $form['name']['#description'] = t('');
+    $form['pass']['#description'] = t('');
 }
 ?>
